8cc66b25d89ffe06b0410d89b85b2fdcbc1a22ee
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2023-04-27 19:56:32 +0300

    commit

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -8,7 +8,7 @@ export const ImageGalleryItem = ({ images, togleModal }) => {
         <li key={item.id} onClick={(evt)=>{togleModal(item.largeImageURL, item.tags);}} className={css.galleryItem}>
           <img
             loading="lazy"
-            className={css.ImageGalleryItem}
+            className={css.imageGalleryItem}
             src={item.webformatURL}
             alt={item.tags}
           />
